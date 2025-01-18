266b0b5e5557a4892330d6915fb9ef8a0a373b32
Keong96 <chinkeongtan96@gmail.com>
2025-01-18 22:08:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -200,6 +200,10 @@ app.post('/user', async (req, res) => {
     });
 });
 
+app.post('/profile/:id', verifyToken, async (req, res) => {
+
+});
+
 app.get('/products', async (req, res) => {
   const { page = 1, limit = 15 , isHot = false, isFeature = false, categoryId} = req.query;
   const offset = (page - 1) * limit;
